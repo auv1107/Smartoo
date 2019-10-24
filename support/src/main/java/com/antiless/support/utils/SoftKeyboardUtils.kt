a1f00ba190b202f1ff0f5b1556f9e610872995a2
antiless <antiless@163.com>
2019-10-24 13:40:24 +0800

    添加键盘高度方法
    Signed-off-by: antiless <antiless@163.com>

--- support/src/main/java/com/antiless/support/utils/SoftKeyboardUtils.kt
+++ support/src/main/java/com/antiless/support/utils/SoftKeyboardUtils.kt
@@ -2,6 +2,10 @@ package com.antiless.support.utils
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.util.Log
+import android.view.View
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
@@ -25,4 +29,65 @@ object SoftKeyboardUtils {
             imm.showSoftInput(editText, 0)
         }
     }
+
+    private var isFirst = true
+
+    interface OnGetSoftHeightListener {
+        fun onShowed(height: Int)
+    }
+
+    interface OnSoftKeyWordShowListener {
+        fun hasShow(isShow: Boolean)
+    }
+
+    /**
+     * 获取软键盘的高度 * *
+     *
+     * @param rootView *
+     * @param listener
+     */
+    fun getSoftKeyboardHeight(rootView: View, listener: OnGetSoftHeightListener?) {
+        val layoutListener: OnGlobalLayoutListener = object : OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                if (isFirst) {
+                    val rect = Rect()
+                    rootView.getWindowVisibleDisplayFrame(rect)
+                    val screenHeight = rootView.rootView.height
+                    val heightDifference = screenHeight - rect.bottom
+                    //设置一个阀值来判断软键盘是否弹出
+
+
+                    val visible = heightDifference > screenHeight / 3
+                    if (visible) {
+                        isFirst = false
+                        listener?.onShowed(heightDifference)
+                        rootView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    }
+                }
+            }
+        }
+        rootView.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
+    }
+
+
+    /**
+     * 判断软键盘是否弹出
+     * * @param rootView
+     *
+     * @param listener 备注：在不用的时候记得移除OnGlobalLayoutListener
+     */
+    fun doMonitorSoftKeyWord(rootView: View, listener: OnSoftKeyWordShowListener?): OnGlobalLayoutListener {
+        val layoutListener = OnGlobalLayoutListener {
+            val rect = Rect()
+            rootView.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = rootView.rootView.height
+            Log.e("TAG", rect.bottom.toString() + "#" + screenHeight)
+            val heightDifference = screenHeight - rect.bottom
+            val visible = heightDifference > screenHeight / 3
+            listener?.hasShow(visible)
+        }
+        rootView.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
+        return layoutListener
+    }
+
 }
